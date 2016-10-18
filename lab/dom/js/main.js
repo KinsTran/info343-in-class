222775544a6342d9ccd7bade7face933cc426ab5
Kinsley Tran <kinstran@uw.edu>
2016-10-18 15:21:18 -0700

    10/18/16 section

--- lab/dom/js/main.js
+++ lab/dom/js/main.js
@@ -6,3 +6,28 @@ var boxOne = document.getElementById('one'),
 	boxFour = document.getElementById('four'),
 	boxFive = document.getElementById('five'),
 	boxSix = document.getElementById('six');
+
+boxOne.onclick = function() {
+	boxOne.classList.add("fade-to-black", "black");
+}
+
+boxTwo.onclick = function() {
+	boxTwo.classList.add("fall");
+}
+
+boxThree.onmouseover = function() {
+	boxThree.classList.add("fade-to-white", "white");
+}
+
+boxThree.onmouseout = function() {
+	boxThree.classList.remove("fade-to-white", "white");
+}
+
+boxFour.onclick = function() {
+	boxFour.classList.toggle("fall");
+}
+
+boxFive.onclick = setInterval(function() { // NEEDS FIXING: CANT GET VALUE FROM STYLE.PADDING
+	console.log("THIS WORKS");
+	boxFive.style.padding = toString(boxFive.style.padding + 100) + "px";
+}, 500);
